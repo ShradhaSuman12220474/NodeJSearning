0b8ac2b07e9d6d78c427b30c0cddbca671563553
suman <isuman0007@gmail.com>
2023-11-11 18:25:02 +0530

    learn math lib

--- basic1/numsandstring.js
+++ basic1/numsandstring.js
@@ -1,2 +1,13 @@
 const num=new Number(34)
 console.log(num.toFixed(2))
+
+const subs=100000
+console.log(subs.toLocaleString())
+console.log(Number.MAX_VALUE)
+
+console.log(Math.max(56,34))//studuy more about the math library
+console.log(Math.random()) //return the no between  0 and 1
+console.log(Math.random()*10) //returns the no betweeen 0 and 9.9
+
+console.log(Math.floor(Math.random()*10)) //it returns the no between 0 to 9 as it prints the floor value
+console.log(Math.floor(Math.random()*10+1))// in this it addes the 1 after calculation
