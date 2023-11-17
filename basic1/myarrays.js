a296df99751b5c3f67268b74f681367a0cdedd8d
suman <isuman0007@gmail.com>
2023-11-18 02:45:01 +0530

    discuss the array

---
+++ basic1/myarrays.js
@@ -0,0 +1,11 @@
+//every things in js is an object
+
+const myarray=[1,2,45,6,]
+//behind the scene this happens
+const myNewarray=new Array(1,2,45,6,7)
+
+const dchearos=["superman" ,"batman", "spiderman", true] //we can add multiple data types
+console.log(dchearos.push("suman"))
+console.log(dchearos)
+dchearos.pop()
+console.log(dchearos);
