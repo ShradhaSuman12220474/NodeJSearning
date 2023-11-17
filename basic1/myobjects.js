a5539dd823506503c0b2a37f380fbc3e70931766
suman <isuman0007@gmail.com>
2023-11-18 03:07:34 +0530

    study about the objects

---
+++ basic1/myobjects.js
@@ -0,0 +1,23 @@
+const user={
+    username:"su",
+    "first name": "suman",//Interview question that we can use space to write the name of keys object
+    name: "shhradha suman",
+    email: "isuman@gmail.com",
+    age: 20,
+    isAdmin: false,
+    isLoggedIn: true,
+    tea:["ginger", "garlic","green"],
+    address:{
+        city:"jamui",
+        state:"Bihar"
+    }
+}
+
+//Now there are two ways to print the objects
+//1) .notation
+//2) []
+
+console.log(user.username)
+console.log(user.tea[1])
+console.log(user.address.city)
+console.log(user["first name"])//this can only be printed in this way only
