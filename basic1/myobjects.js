881977bbcbefdc3802101969295368384016b678
suman <isuman0007@gmail.com>
2023-11-18 16:56:42 +0530

    data types are done

--- basic1/myobjects.js
+++ basic1/myobjects.js
@@ -21,3 +21,9 @@ console.log(user.username)
 console.log(user.tea[1])
 console.log(user.address.city)
 console.log(user["first name"])//this can only be printed in this way only
+delete user.isAdmin
+const userListFromDatabase=[{},{},{}]
+console.log(Object.keys(user))
+console.log(Object.values(user))
+console.log(Object.entries(user))
+console.log(user.hasOwnProperty('keyboard'))//to check whether the key is available in the object or not
